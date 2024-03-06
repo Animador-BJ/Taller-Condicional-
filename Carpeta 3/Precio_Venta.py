e240ac838d5098d6e26b250fccad81961cfc01ce
jonathanbenitezmayorga <jonathanbenitez@colegioguanena.edu.co>
2024-03-06 07:01:15 -0500

    Se actualizo la carpera 3 y ya esta terminada

--- Carpeta 3/Precio_Venta.py
+++ Carpeta 3/Precio_Venta.py
@@ -1,20 +1,20 @@
 # Programa que indica el precio de venta de un articulo dado
 
-# imput
-Precio_costo = int(input("lo que vale el articulo: "))
+# Input
+Precio_costo = int(input("Ingrese el costo del artículo: "))
 
-# processing
-
+# Processing
 if Precio_costo < 3000:
     ganancia = Precio_costo * 0.15
     print(f"La ganancia es: {ganancia}")
+    print(f"Precio_costo + {ganancia} = {Precio_costo + ganancia}")
     
 elif Precio_costo < 6000:
-     ganancia = 500
-     print(f"La ganancia es: {ganancia}")
-    
+    ganancia = 500
+    print(f"La ganancia es: {ganancia}")
+    print(f"Precio_costo + {ganancia} = {Precio_costo + ganancia}")
     
 else:
     ganancia = Precio_costo * 0.25
-    print(f"La ganancia es: {ganancia} ") 
-    
+    print(f"La ganancia es: {ganancia}")
+    print(f"Precio_costo + {ganancia} = {Precio_costo + ganancia}")
