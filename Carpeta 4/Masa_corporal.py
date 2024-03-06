0e08258b0a2b7473b70c9cd6f76f12ac8c08a913
jonathanbenitezmayorga <jonathanbenitez@colegioguanena.edu.co>
2024-03-06 08:41:40 -0500

    Se actualizo la carpera 4 pero falta acabarle le redme

---
+++ Carpeta 4/Masa_corporal.py
@@ -0,0 +1,28 @@
+# Programa que indica el IMC y su clasificación
+
+# Input
+Peso = int(input("Ingrese su peso actual: "))
+Estatura = float(input("Ingrese su estatura actual: "))
+
+# Processing
+IMC = Peso / Estatura**2
+
+if IMC < 16:
+    Resultado = "Criterio de ingreso en hospital"
+elif IMC <= 17:
+    Resultado = "Infrapeso"
+elif IMC <= 18.5:
+    Resultado = "Bajo peso"
+elif IMC <= 25:
+    Resultado = "Peso normal (Saludable)"
+elif IMC <= 30:
+    Resultado = "Sobrepeso (Obesidad de grado 1)"
+elif IMC <= 35:
+    Resultado = "Sobrepeso crónico (Obesidad de grado 2)"
+elif IMC <= 40:
+    Resultado = "Obesidad premórbida (Obesidad de grado 3)"
+elif IMC > 40:
+    Resultado = "Obesidad mórbida (Obesidad de grado 4)"
+
+# Output
+print(f"Su IMC es {IMC:.2f} y sus resultados son: {Resultado}")
--- Carpeta 4/README.md
+++ Carpeta 4/README.md
@@ -1,2 +1,2 @@
-# Taller-Condicional-
-Taller de condicionales
+# Masa Corporal
+## Programa que calcula el indice de masa corporal "IMC" de una persona por medio de su peso y estatura
